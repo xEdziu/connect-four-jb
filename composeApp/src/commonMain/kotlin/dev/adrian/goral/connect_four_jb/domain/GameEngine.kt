f20d27028842c7894c6553f9eba58bf77076ccd1
Adrian Goral <adrian.goral@gmail.com>
2026-04-23 23:29:16 +0200

    feat: implement GameEngine

---
+++ composeApp/src/commonMain/kotlin/dev/adrian/goral/connect_four_jb/domain/GameEngine.kt
@@ -0,0 +1,47 @@
+package dev.adrian.goral.connect_four_jb.domain
+
+class GameEngine(initialState: GameState = GameState()) {
+    var gameState: GameState = initialState
+        private set
+
+    fun play(column: Int) {
+        val current = gameState
+        if (current.status != GameStatus.IN_PROGRESS) return
+
+        val player = current.currentPlayer
+        val updatedBoard = current.board.dropToken(column, player)
+
+        if (WinValidator.hasWon(updatedBoard, player, current.config)) {
+            gameState = current.copy(
+                board = updatedBoard,
+                status = GameStatus.WON,
+                winner = player,
+                currentPlayer = player
+            )
+            return
+        }
+
+        if (isBoardFull(updatedBoard)) {
+            gameState = current.copy(
+                board = updatedBoard,
+                status = GameStatus.DRAW,
+                winner = Player.NONE
+            )
+            return
+        }
+
+        gameState = current.copy(
+            board = updatedBoard,
+            currentPlayer = player.next()
+        )
+    }
+
+    private fun isBoardFull(board: Board): Boolean =
+        board.grid.all { row -> row.none { cell -> cell == Player.NONE } }
+
+    private fun Player.next(): Player = when (this) {
+        Player.RED -> Player.BLUE
+        Player.BLUE -> Player.RED
+        Player.NONE -> Player.NONE
+    }
+}
